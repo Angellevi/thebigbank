c48c85e95c886b140bf162d9c6a2550412520c08
Angellevi <angel.levente@gmail.com>
2021-06-03 17:54:04 +0200

    asd

---
+++ js/kalkulatorok.js
@@ -0,0 +1,7 @@
+function hitelKalkulalas() {
+    const menubol = document.getElementById("dropdown_menu_1");
+    const mennyiseg = document.getElementById("hitelSzam");
+    const futamido = document.getElementById("futamido_1");
+
+    console.log(menubol, mennyiseg, futamido);
+}
